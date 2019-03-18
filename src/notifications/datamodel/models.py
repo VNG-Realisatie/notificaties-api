37362fb66ce2c72567447b265f7cfedaa70e0c66
Joeri Bekker <joeri@maykinmedia.nl>
2019-03-18 18:46:12 +0100

    Small model changes to have Dutch labels.

--- src/notifications/datamodel/models.py
+++ src/notifications/datamodel/models.py
@@ -1,25 +1,26 @@
 import uuid as _uuid
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Kanaal(models.Model):
     uuid = models.UUIDField(
         unique=True, default=_uuid.uuid4,
-        help_text='Unique recource identifier (UUID4)'
+        help_text=_('Unique recource identifier (UUID4)')
     )
     naam = models.CharField(
-        max_length=50, help_text='name of the channel/exchange',
-        unique=True
+        _('Naam'), max_length=50, unique=True,
+        help_text=_('Naam van het kanaal (ook wel "Exchange" genoemd) dat de bron vertegenwoordigd.')
     )
     documentatie_link = models.URLField(
-        help_text='Url of subscriber API to which NC will post messages',
-        null=True
+        _('Documentatie link'), blank=True,
+        help_text=_('URL naar documentatie.'),
     )
 
-    class Mets:
-        verbose_name = 'kanaal'
-        verbose_name_plural = 'kanalen'
+    class Meta:
+        verbose_name = _('kanaal')
+        verbose_name_plural = _('kanalen')
 
     def __str__(self) -> str:
         return f"{self.naam}"
@@ -28,20 +29,25 @@ class Kanaal(models.Model):
 class Abonnement(models.Model):
     uuid = models.UUIDField(
         unique=True, default=_uuid.uuid4,
-        help_text='Unique recource identifier (UUID4)'
+        help_text=_('Unique recource identifier (UUID4)')
     )
     callback_url = models.URLField(
-        help_text='Url of subscriber API to which NC will post messages',
-        unique=True
+        _('Callback URL'), unique=True,
+        help_text=_('De URL waar notificaties naar toe gestuurd dienen te worden. Deze URL dient uit te komen bij een '
+                  'API die geschikt is om notificaties op te ontvangen.')
     )
     auth = models.CharField(
-        max_length=1000, blank=True,
-        help_text='Authentication method to subscriber'
+        _('Autorisatie header'), max_length=1000, blank=True,
+        help_text=_('Autorisatie header invulling voor het vesturen naar de "Callback URL". Voorbeeld: Bearer '
+                    'a4daa31...')
     )
 
     class Meta:
-        verbose_name = 'abonnement'
-        verbose_name_plural = 'abonnementen'
+        verbose_name = _('abonnement')
+        verbose_name_plural = _('abonnementen')
+
+    def __str__(self) -> str:
+        return f"{self.uuid}"
 
     @property
     def kanalen(self):
@@ -55,9 +61,12 @@ class FilterGroup(models.Model):
     abonnement = models.ForeignKey(Abonnement, on_delete=models.CASCADE, related_name='filter_groups')
     kanaal = models.ForeignKey(Kanaal, on_delete=models.CASCADE, related_name='filter_groups')
 
+    class Meta:
+        verbose_name = _('filter')
+        verbose_name_plural = _('filters')
+
     def match_pattern(self, msg_filters):
-        abon_filters = self.filters.order_by('id')
-        for f in zip(abon_filters, msg_filters):
+        for f in zip(self.filters.all(), msg_filters):
             abon_filter, msg_filter = f
             msg_key = list(msg_filter)[0]
             if not (
@@ -70,10 +79,19 @@ class FilterGroup(models.Model):
 
 
 class Filter(models.Model):
-    key = models.CharField(max_length=100)
-    value = models.CharField(max_length=1000)
+    key = models.CharField(
+        _('Sleutel'), max_length=100
+    )
+    value = models.CharField(
+        _('Waarde'), max_length=1000
+    )
     filter_group = models.ForeignKey(FilterGroup, on_delete=models.CASCADE, related_name='filters')
     # internal_increment = models.IntegerField(help_text="field to simplify filtering topics")
 
     def __str__(self) -> str:
         return f"{self.key}: {self.value}"
+
+    class Meta:
+        ordering = ('id', )
+        verbose_name = _('filter-onderdeel')
+        verbose_name_plural = _('filter-onderdelen')
