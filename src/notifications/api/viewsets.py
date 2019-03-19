08d6eb9ef0782be4f95540e385394ad5a850ded4
Alvise <alvise@maykinmedia.nl>
2019-03-19 16:44:55 +0100

    revert swagger schema hidden

--- src/notifications/api/viewsets.py
+++ src/notifications/api/viewsets.py
@@ -56,7 +56,7 @@ class KanaalViewSet(CheckQueryParamsMixin,
 
 class NotificatieAPIView(views.APIView):
     # TODO: to remove when the vng-api-common will be updated
-    # swagger_schema = None
+    swagger_schema = None
 
     parser_classes = (JSONParser,)
     permission_classes = (ActionScopesRequired,)
