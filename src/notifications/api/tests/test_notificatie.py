6e938b251c25a22f67526ebe48ce08b40f67a237
Alvise <alvise@maykinmedia.nl>
2019-03-21 12:50:17 +0100

    Add test for notificatie lgo

--- src/notifications/api/tests/test_notificatie.py
+++ src/notifications/api/tests/test_notificatie.py
@@ -13,6 +13,7 @@ from vng_api_common.tests import JWTScopesMixin, get_operation_url
 from notifications.datamodel.tests.factories import (
     AbonnementFactory, FilterFactory, FilterGroupFactory, KanaalFactory
 )
+from notifications.datamodel.models import Notificatie
 
 from ..channels import QueueChannel
 from ..scopes import SCOPE_NOTIFICATIES_PUBLICEREN
@@ -97,3 +98,37 @@ class NotificatieTests(JWTScopesMixin, APITestCase):
                 'Authorization': abon.auth
             }
         )
+
+    @patch('notifications.api.serializers.requests.post')
+    @patch.object(QueueChannel, 'send')
+    def test_notificatie_log(self, mock_queue, mock_post):
+        """
+        test /notificatie POST:
+        check if message was send to subscribers callbackUrls
+
+        """
+        notificaties = Notificatie.objects.all()
+        kanaal = KanaalFactory.create(naam='zaken')
+        abon = AbonnementFactory.create(callback_url='https://example.com/callback')
+        filter_group = FilterGroupFactory.create(kanaal=kanaal, abonnement=abon)
+        filter = FilterFactory.create(filter_group=filter_group, key='bron', value='082096752011')
+        notificatie_url = reverse('notificaties-list',
+                                  kwargs={'version': BASE_REST_FRAMEWORK['DEFAULT_VERSION']})
+        request_data = {
+            "kanaal": "zaken",
+            "hoofdObject": "https://ref.tst.vng.cloud/zrc/api/v1/zaken/d7a22",
+            "resource": "status",
+            "resourceUrl": "https://ref.tst.vng.cloud/zrc/api/v1/statussen/d7a22/721c9",
+            "actie": "create",
+            "aanmaakdatum": "2018-01-01T17:00:00Z",
+            "kenmerken": [
+                {"bron": "082096752011"},
+                {"zaaktype": "example.com/api/v1/zaaktypen/5aa5c"},
+                {"vertrouwelijkheidaanduiding": "openbaar"}
+            ]
+        }
+
+        response = self.client.post(notificatie_url, request_data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
+        self.assertEqual(len(notificaties), len(Notificatie.objects.all()) + 1)
