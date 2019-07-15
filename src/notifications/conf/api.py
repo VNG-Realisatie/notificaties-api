524eeb7f01d19344875d6b68d32c59dc59c8fc1f
Sergei Maertens <sergei@maykinmedia.nl>
2019-07-15 10:53:48 +0200

    :arrow_up: Bump vng-api-common

--- src/notifications/conf/api.py
+++ src/notifications/conf/api.py
@@ -11,9 +11,6 @@ SECURITY_DEFINITION_NAME = 'JWT-Claims'
 
 SWAGGER_SETTINGS = BASE_SWAGGER_SETTINGS.copy()
 
-_default_field_inspectors = list(BASE_SWAGGER_SETTINGS['DEFAULT_FIELD_INSPECTORS'])
-_default_field_inspectors.remove('vng_api_common.inspectors.geojson.GeometryFieldInspector')
-
 SWAGGER_SETTINGS.update({
     'DEFAULT_INFO': 'notifications.api.schema.info',
 
@@ -32,8 +29,6 @@ SWAGGER_SETTINGS.update({
             # 'type': 'apiKey',
         }
     },
-
-    'DEFAULT_FIELD_INSPECTORS': tuple(_default_field_inspectors),
 })
 
 GEMMA_URL_INFORMATIEMODEL_VERSIE = '1.0'
