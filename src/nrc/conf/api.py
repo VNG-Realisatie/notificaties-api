02dd9201b3bd6caeffc79aa2e1ce81143a4e6d9a
Sonny Bakker <sonny@maykinmedia.nl>
2022-06-08 16:32:53 +0200

    Update version number

--- src/nrc/conf/api.py
+++ src/nrc/conf/api.py
@@ -2,7 +2,7 @@ import os
 
 from vng_api_common.conf.api import *  # noqa - imports white-listed
 
-API_VERSION = "2.0.0-alpha4"
+API_VERSION = "2.0.0-alpha5"
 
 REST_FRAMEWORK = BASE_REST_FRAMEWORK.copy()
 REST_FRAMEWORK.update(
