f23fc36759f28942fc342ba3b63597aca611a675
Sonny Bakker <sonny@maykinmedia.nl>
2022-06-14 13:08:24 +0200

    [#36] update event documentation

--- src/nrc/api/serializers.py
+++ src/nrc/api/serializers.py
@@ -86,43 +86,75 @@ class SubscriptionSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class EventSerializer(serializers.Serializer):
-    id = serializers.CharField(help_text=_("ID van het EVENT."))
+    id = serializers.CharField(
+        help_text=_(
+            "Identifies the event. Producers MUST ensure that source + id is "
+            "unique for each distinct event. SHOULD be a UUID."
+        )
+    )
 
     specversion = serializers.ChoiceField(
         help_text=_(
-            "De versie van de CloudEvents specificatie welke het EVENT gebruikt."
+            "The version of the CloudEvents specification which the event uses. "
+            "Compliant event producers MUST use a value of 1.0 when referring to "
+            "this version of the specification."
         ),
         choices=SpecVersionChoices.choices,
     )
 
     source = serializers.CharField(
-        help_text=_("Identificeert de context waarin een EVENT heeft plaatsgevonden."),
+        help_text=_(
+            "Identifies the context in which an event happened. SHOULD be a URN "
+            "notation with 'nld' as namespace identifier. SHOULD contain consecutive "
+            "a unique identifier of the organization that publishes the event followed "
+            "by the source system that publishes the event. Involved organizations "
+            "SHOULD agree on how organizations and systems are uniquely identified "
+            "(e.g. via the use of OIN, KVK-nummer or eIDAS legal identifier for "
+            "organization identification);."
+        ),
     )
 
     domain = serializers.ModelField(
-        help_text=_("Naam van het DOMAIN waartoe het EVENT behoort."),
+        help_text=_(
+            "Name of the domain to which the event belongs. Can be seen as the "
+            "namespace of the event.(This attribute is not listed in the GOV NL "
+            "profile for CloudEvents)."
+        ),
         model_field=Domain()._meta.get_field("name"),
     )
 
     type = serializers.CharField(
-        help_text=_("Beschrijft het type EVENT afkomstig van het specifieke DOMAIN."),
+        help_text=_(
+            "Beschrijft het type EVENT afkomstig van het specifieke DOMAIN."
+            "This attribute contains a value describing the type of event. Type "
+            "SHOULD start with the domain followed by the name of the event. Events "
+            "SHOULD be expressed in the past tense. If subtypes are required those "
+            "SHOULD be expressed using a dot '.' between the super and subtype(s). "
+            "The type MAY contain version information. Version information SHOULD "
+            "be appended at the end of the string."
+        ),
     )
 
     time = serializers.DateTimeField(
-        help_text=_("Tijdstempel van wanneer het EVENT heeft plaatgevonden."),
+        help_text=_(
+            "Timestamp of the event. SHOULD be the timestamp the event was registered "
+            "in the source system and NOT the time the event occurred in reality. "
+            "The exact meaning of time MUST be clearly documented."
+        ),
         required=False,
         allow_null=True,
     )
 
     subscription = serializers.UUIDField(
         help_text=_(
-            "De gebeurtenis is naar de API gepost omdat aan de filtercriteria van"
-            " deze SUBSCRIPTION is voldaan. De uuid verwijst naar een SUBSCRIPTION op"
-            " de bron die deze EVENT heeft gepubliceerd. Het moet worden"
-            " doorgegeven wanneer dit EVENT wordt afgeleverd bij SUBSCRIPTIONs."
-            " Wanneer een EVENT wordt gedistribueerd naar een SUBSCRIPTION, moet"
-            " dit kenmerk worden overschreven (of ingevuld) met de SUBSCRIPTION's"
-            " uuid van de abonnee die de levering heeft geactiveerd."
+            "Usually empty. Only used in situations where notificationservices are "
+            "chained. For example notificationservice2 (ns2) is subscribed to "
+            "notifcationservice1 (ns1). When ns1 sends an event to ns2 this attribute "
+            "SHOULD contain the subscription id of the subscription that ns1 has "
+            "on ns2 (that was resposible for receiving the event). Note this "
+            "attribute is overwritten when the event is passed through to a client. "
+            "It will be set to the value of the subscription id of the subscription "
+            "of the client."
         ),
         required=False,
         allow_null=True,
@@ -131,13 +163,14 @@ class EventSerializer(serializers.Serializer):
     # TODO: we should pick either camelcase (like other ZGW components) or allow underscores
     subscriberReference = serializers.CharField(
         help_text=_(
-            "De gebeurtenis is naar de API gepost omdat aan de filtercriteria van"
-            " deze SUBSCRIPTION is voldaan. De uuid verwijst naar een SUBSCRIPTION op"
-            " de bron die deze EVENT heeft gepubliceerd. Het moet worden"
-            " doorgegeven wanneer dit EVENT wordt afgeleverd bij SUBSCRIPTIONs."
-            " Wanneer een EVENT wordt gedistribueerd naar een SUBSCRIPTION, moet"
-            " dit kenmerk worden overschreven (of ingevuld) met de SUBSCRIPTION's"
-            " uuid van de abonnee die de levering heeft geactiveerd."
+            "Usually empty. Only used in situations where notificationservices are "
+            "chained. For example notificationservice2 (ns2) is subscribed to "
+            "notifcationservice1 (ns1). When ns1 sends an event to ns2 this attribute "
+            "COULD contain the subscriberReference the was specified when ns2 "
+            "subscribed to ns1. Note this attribute is overwritten when the event "
+            "is passed through to a client. It will be set to the value of the "
+            "subscriberReference of the subscription of the client (when specified "
+            "by the client)."
         ),
         max_length=255,
         required=False,
@@ -145,20 +178,24 @@ class EventSerializer(serializers.Serializer):
     )
 
     datacontenttype = serializers.CharField(
-        help_text=_("Content-type van de meegegeven data."),
+        help_text=_(
+            "Content type of data value. In this version of the API the value MUST "
+            "be 'application/json'. In future versions of the API other values "
+            "such as described in RFC 2046 MAY be used."
+        ),
         required=False,
         allow_null=True,
     )
     dataschema = serializers.URLField(
-        help_text=_("Identificeert het schema waarmee de data gevalideerd kan worden."),
+        help_text=_("Identifies the schema that data adheres to."),
         required=False,
         allow_null=True,
     )
 
     sequence = serializers.CharField(
         help_text=_(
-            "Volgorde van het EVENT. Dit maakt het mogelijk meerdere opeenvolgende"
-            " EVENTs te versturen."
+            "Value expressing the relative order of the event. This enables "
+            "interpretation of data supercedence."
         ),
         required=False,
         allow_null=True,
@@ -166,21 +203,42 @@ class EventSerializer(serializers.Serializer):
 
     sequencetype = serializers.ChoiceField(
         choices=SequencetypeChoices.choices,
-        help_text=_("Specificeert het type van de opgegeven volgorde."),
+        help_text=_(
+            "Specifies the semantics of the sequence attribute. (Currently limited "
+            "to the value INTEGER)."
+        ),
         required=False,
         allow_null=True,
     )
 
-    subject = serializers.CharField(required=False, allow_null=True)
+    subject = serializers.CharField(
+        help_text=_(
+            "Included to be compatible with CloudEvents specification. The GOV NL "
+            "profile states 'Decision on whether or not to use the attribute and/or "
+            "the exact interpretation is postponed. To be determined partly on the "
+            "basis of future agreements about subscription and filtering.'"
+        ),
+        required=False,
+        allow_null=True,
+    )
 
     data = serializers.JSONField(required=False, allow_null=True)
     data_base64 = serializers.CharField(
-        validators=[Base64Validator()], required=False, allow_null=True
+        validators=[Base64Validator()],
+        help_text=_(
+            "The presence of the data_base64 member clearly indicates that the "
+            "value is a Base64 encoded binary data, which the serializer MUST decode "
+            "into a binary runtime data type."
+        ),
+        required=False,
+        allow_null=True,
     )
 
     dataref = serializers.CharField(
         help_text=_(
-            "Een referentie naar een locatie waar de data van het EVENT is opgeslagen."
+            "A reference to a location where the event payload is stored. If both "
+            "the data attribute and the dataref attribute are specified their "
+            "contents MUST be identical."
         ),
         required=False,
         allow_null=True,
